076174ec8a4655a1c96e292ae9ea9363a86ef55b
crisostomi <donatocrisostomi@gmail.com>
2025-08-21 09:49:29 +0200

    new stuff

---
+++ commands.sh
@@ -0,0 +1,4 @@
+HYDRA_FULL_ERROR=1 HF_HUB_OFFLINE=1 uv run evaluate -m 'nn/benchmark=twenty' 'nn/module/encoder=b32' 'nn.module.router.threshold=0.05,0.1,0.15,0.2,0.3' 'nn.module.router.max_num_tasks_to_select=1,2,3,4,5'
+
+
+HYDRA_FULL_ERROR=1 HF_HUB_OFFLINE=1 uv run evaluate -m 'nn/benchmark=twenty' 'nn/module/encoder=b16' 'similarity_threshold=-0.4,-0.2,0.0,0.1,0.2,0.4,0.6,0.8' 
