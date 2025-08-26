b43f1cd98f668480a88d6e24e018948e381aadf7
crisostomi <donatocrisostomi@gmail.com>
2025-08-26 14:04:09 +0200

    prepare for circuits

---
+++ src/mass/merger/circuits.py
@@ -0,0 +1,55 @@
+import copy
+from mass.merger.merger import TaskVectorBasedMerger
+from mass.modules.encoder import ImageEncoder
+from mass.utils.utils import apply_dict_to_model, compute_task_dict, print_memory
+from mass.task_vectors.task_singular_vectors import (
+    get_svd_dict,
+    isotropic_sum,
+    sum_svd,
+    sum_svd_no_redundant_tasks_simple,
+)
+
+import torch
+
+
+# TODO: @tensorqt change this
+class CircuitsMerger(TaskVectorBasedMerger):
+
+    def __init__(self, svd_path, svd_compress_factor):
+        super().__init__()
+
+        self.svd_path = svd_path
+        self.svd_compress_factor = svd_compress_factor
+
+    def merge(self, base_model, finetuned_models):
+
+        task_dicts = {}
+
+        datasets = list(finetuned_models.keys())
+
+        for dataset in datasets:
+            task_dicts[dataset] = compute_task_dict(
+                base_model.state_dict(), finetuned_models[dataset]
+            )
+            del finetuned_models[dataset]  # Delete one model at a time
+            torch.cuda.empty_cache()
+
+        print_memory("after computing task dicts")
+
+        svd_dict = get_svd_dict(
+            task_dicts, datasets, self.svd_path, self.svd_compress_factor
+        )
+
+        multi_task_vector = sum_svd(
+            ref_state_dict=copy.deepcopy(base_model.state_dict()),
+            svd_dicts=svd_dict,
+        )
+
+        merged_encoder: ImageEncoder = copy.deepcopy(base_model)
+
+        merged_encoder = apply_dict_to_model(
+            multi_task_vector,
+            merged_encoder,
+        )
+
+        return merged_encoder
